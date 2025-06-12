e583b8f45882a4e2ca50404455ef0a678f71916f
Carl Edman <CarlEdman@gmail.com>
2025-06-12 08:56:51 -0400

    More skeletons.

--- src/benderchef/benderchef.py
+++ src/benderchef/benderchef.py
@@ -1,20 +1,49 @@
 #! python3
-import plexapi
+import logging
+
 import click
+import plexapi
 from plexapi.myplex import MyPlexAccount, PlexServer
 
 
 @click.group()
 @click.version_option()
 @click.option(
-    "-c", "--clobber/--no-clobber", default=False
-)  # "if necessary, overwrite existing items."
+    "-c",
+    "--clobber/--no-clobber",
+    default=False,
+    help="if necessary, overwrite existing items.",
+)
 @click.option(
-    "-m", "--move/--copy", default=False
-)  # "delete originals of transferred items."
+    "--move/--copy",
+    "move",
+    default=False,
+    help="move items.  (i.e., remove original items if successfully transferred elsewhere).",
+)
 @click.option(
-    "-d", "--dryrun/--live", default=False
-)  #  "do not perform operations, but only print them.
+    "--dryrun",
+    "dryrun",
+    default=False,
+    help="do not perform operations, but only print them.",
+)
+@click.option(
+    "--debug",
+    "loglevel",
+    flag_value=logging.DEBUG,
+    help="print debugging (or higher level) log messages.",
+)
+@click.option(
+    "--verbose",
+    "loglevel",
+    flag_value=logging.VERBOSE,
+    help="print informational (or higher level) log messages.",
+)
+@click.option(
+    "--taciturn",
+    "loglevel",
+    flag_value=logging.ERROR,
+    help="print only error (or higher level) log messages.",
+)
 @click.option("--user-name", type=str)
 @click.option("--password", type=str)
 @click.option("--server-base-url", type=str)
@@ -35,6 +64,7 @@ def main(
     client_token,
     container_size,
     timeout,
+    loglevel,
 ):
     """CLI to Plex REST API"""
     click.echo("CLI")
@@ -60,12 +90,13 @@ def watchlist_export(token):
     click.echo("export")
     account = MyPlexAccount()
     for i in account.watchlist(sort="name:asc"):
-      click.echo(repr(i))
+        click.echo(repr(i))
+
+
 #    plex = account.resource('<SERVERNAME>').connect()  # returns a PlexServer instance
 # plex = PlexServer()
-#for video in plex.library.section("Movies").search(unwatched=True):
+# for video in plex.library.section("Movies").search(unwatched=True):
 #    print(video.title)
-
 
 
 @watchlist.command("transfer")
@@ -76,26 +107,47 @@ def watchlist_transfer(from_token, to_token):
     click.echo("transfer")
 
 
+@main.group()
+def history():
+    """Manage users' local history."""
+    click.echo("history")
+
+
+@history.command("import")
+@click.argument("token", type=int)
+def history_import(token):
+    """Ingest watch history to User TOKEN from stdin in JSON format."""
+    click.echo("import")
+
+
+@history.command("export")
+@click.argument("token", type=int)
+def history_export(token):
+    """Dump a watch history of User TOKEN to stdout in JSON format."""
+    click.echo("export")
+    # account = MyPlexAccount()
+    # for i in account.history(sort="name:asc"):
+    #     click.echo(repr(i))
+
+
+#    plex = account.resource('<SERVERNAME>').connect()  # returns a PlexServer instance
+# plex = PlexServer()
+# for video in plex.library.section("Movies").search(unwatched=True):
+#    print(video.title)
+
+
+@history.command("transfer")
+@click.argument("from_token", type=int)
+@click.argument("to_token", type=int)
+def history_transfer(from_token, to_token):
+    """Move all history from User FROM_TOKEN to User TO_TOKEN."""
+    click.echo("transfer")
+
+
 # log = logging.getLogger()
 
 # parser = argparse.ArgumentParser(
 #     fromfile_prefix_chars="@", prog=prog, epilog="Written by: " + author
-# )
-
-# parser.add_argument(
-#     "--debug",
-#     dest="loglevel",
-#     action="store_const",
-#     const=logging.DEBUG,
-#     help="print debugging (or higher) log messages.",
-# )
-
-# parser.add_argument(
-#     "--taciturn",
-#     dest="loglevel",
-#     action="store_const",
-#     const=logging.ERROR,
-#     help="only print error level (or higher) log messages.",
 # )
 
 # parser.add_argument(
